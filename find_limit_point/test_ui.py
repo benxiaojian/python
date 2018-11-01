40901fd5316cb8fd68f9bde7721d5d68c5a8dd20
nine <445376588@qq.com>
2018-11-01 22:39:21 +0800

    add picture successfully

--- find_limit_point/test_ui.py
+++ find_limit_point/test_ui.py
@@ -8,7 +8,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 from PyQt5.QtWidgets import *
-from PyQt5 import *
 
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
@@ -19,24 +18,24 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(827, 622)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(0, 30, 781, 531))
         self.label.setObjectName("label")
         self.open_file = QtWidgets.QPushButton(self.centralwidget)
-        self.open_file.setGeometry(QtCore.QRect(10, 0, 80, 23))
+        self.open_file.setGeometry(QtCore.QRect(20, 0, 80, 23))
         self.open_file.setObjectName("open_file")
+        self.print_lines = QtWidgets.QPushButton(self.centralwidget)
+        self.print_lines.setGeometry(QtCore.QRect(150, 0, 80, 23))
+        self.print_lines.setObjectName("print_lines")
         self.eight_lines = QtWidgets.QPushButton(self.centralwidget)
-        self.eight_lines.setGeometry(QtCore.QRect(150, 0, 80, 23))
+        self.eight_lines.setGeometry(QtCore.QRect(290, 0, 80, 23))
         self.eight_lines.setObjectName("eight_lines")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(290, 0, 80, 23))
-        self.pushButton_3.setObjectName("pushButton_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 827, 20))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -48,18 +47,22 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
         self.open_file.clicked.connect(self.openFile)
 
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
         self.open_file.setText(_translate("MainWindow", "open"))
-        self.eight_lines.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_3.setText(_translate("MainWindow", "PushButton"))
+        self.print_lines.setText(_translate("MainWindow", "print line"))
+        self.eight_lines.setText(_translate("MainWindow", "eight_lines"))
 
 
     def openFile(self):
-        file_name = QFileDialog.getOpenFileName(self, 'open file', '', 'Picture File(*.jpg, *.jpeg, *.png)')
-        pix = QPixmap(file_name)
+        img_name, img_type= QFileDialog.getOpenFileName(self, "打开图片", "", " *.jpg;;*.png;;*.jpeg;;*.bmp;;All Files (*)")
+        if img_name == "":
+            return
+
+        pix = QtGui.QPixmap(img_name).scaled(self.label.width(), self.label.height())
         self.label.setPixmap(pix)
 
 
